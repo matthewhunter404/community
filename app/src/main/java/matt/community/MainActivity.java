4bf0ff38f7b2b712b890cd8c01b0baa2c50b9766
Matthew <matthewhunter404@gmail.com>
2016-09-11 23:14:27 +0300

    Adds picasso as an image handling library and uses it to laod the background image. This drasticaly reduces the procesing workload created by this image. Leaves some overall image scaling bugs to sort out however.

--- app/src/main/java/matt/community/MainActivity.java
+++ app/src/main/java/matt/community/MainActivity.java
@@ -3,6 +3,7 @@ package matt.community;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,6 +13,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -28,6 +32,20 @@ public class MainActivity extends AppCompatActivity
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
+
+        //Sets the background to an image using picasso. Image resizing etc takes place here automatically
+        DisplayMetrics displaymetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displaymetrics);
+        ImageView backgroundImage = (ImageView) findViewById(R.id.main_background);
+        int displayWidth = displaymetrics.widthPixels;
+        int displayHeight = displaymetrics.heightPixels;
+        int imageHeight=backgroundImage.getMeasuredHeight();
+        int imageWidth=backgroundImage.getMeasuredWidth();
+        Picasso.with(this)
+                .load(R.drawable.main_background)
+                .resize(500, 500)
+                .centerCrop()
+                .into(backgroundImage);
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
